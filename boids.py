93f6b4a749a3d87e5ed26b16b5a52db732eaf1bc
smorrel1 <stephen.morrell@gmail.com>
2017-02-24 14:55:56 +0000

    tidy up boids

--- boids.py
+++ boids.py
@@ -76,17 +76,13 @@ def update_boids(boids):
 
 flark = yaml.load(open("/Users/stephenmorrell/git/bad-boids/config.yml"))
 boids = instantiate_boids(**flark)
-# boids = instantiate_boids(x_min=-450.0, x_max=50.0, y_min=300.0, y_max=600.0, n_boids=50, x_vel_min=0.0, x_vel_max=10.0,
-#                       y_vel_min=-20.0, y_vel_max=20.0)
 figure = plt.figure()
 axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
 scatter = axes.scatter(boids[0], boids[1])
 
-
 def animate(frame):
   update_boids(boids)
   scatter.set_offsets(zip(boids[0], boids[1]))
-
 
 anim = animation.FuncAnimation(figure, animate,
                                frames=50, interval=50)
